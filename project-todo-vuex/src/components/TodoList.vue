d26afaf916f42c05cfbbf0419247593ce7e9cabe
Youngsim Cha <chayoungsim@gmail.com>
2019-04-04 17:51:02 +0900

    todo-vuex update

---
+++ project-todo-vuex/src/components/TodoList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <transition-group name="list" tag="ul">
+      <li v-for="(todoItem,index) in this.$store.state.todoItems" :key="todoItem.item" class="shadow">
+        <i class="checkBtn fas fa-check" aria-hidden="true" v-bind:class="{checkBtnCompleted:todoItem.completed}" v-on:click="toggleComplete(todoItem,index)"></i><span v-bind:class="{textCompleted:todoItem.completed}">{{todoItem.item}}</span>
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem,index)"><i class="far fa-trash-alt" aria-hidden="true"></i></span>
+      </li>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default { 
+  methods:{
+    removeTodo(todoItem,index){
+      //this.$emit('removeTodo',todoItem,index);
+      this.$store.commit('removeOneItem',{todoItem,index});
+    },
+    toggleComplete(todoItem, index){      
+      //this.$emit('toggleItem', todoItem,index);
+      this.$store.commit('toggleOneItem',{todoItem,index})
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+  ul{
+    list-style-type:none;
+    padding-left:0;
+    margin-top:0;
+    text-align:left;
+  }
+  li{
+    display:flex;
+    min-height:50px;
+    height:50px;;
+    line-height:50px;
+    margin:0.5rem 0;
+    padding: 0 0.9rem;
+    background:#fff;
+    border-radius:5px;
+  }
+  .checkBtn{
+    line-height:45px;
+    color:#62acde;
+    margin-right:5px;
+  }
+  .removeBtn{
+    margin-left:auto;
+    color:#de4343;
+  }
+
+  .list-enter-active, .list-leave-active{
+    transition:all 1s;
+  }
+  .list-enter, .list-leave-to{
+    opacity:0;
+    transform:translateY(30px);
+  }
+  .checkBtnCompleted{
+    color:#b3adad;
+  }
+  .textCompleted{
+    text-decoration: line-through;
+    color:#b3adad;
+  }
+
+</style>
